fe13c26b3509351386cf81068408e7dbcf139dd1
Kamen Yotov <kamen@yotov.org>
2021-04-14 21:44:22 -0400

    refactor package name function

---
+++ utils/KyDepsPackage.cmake
@@ -0,0 +1,14 @@
+include_guard(GLOBAL)
+
+function(get_package_name DEPS_GIT_REPO_DIRECTORY DEPS_LIST OUTPUT_VARIABLE)
+
+    execute_process(
+            COMMAND git rev-parse HEAD
+            WORKING_DIRECTORY ${DEPS_GIT_REPO_DIRECTORY}
+            OUTPUT_VARIABLE GIT_REVISION
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+
+    set(${OUTPUT_VARIABLE} "${GIT_REVISION}-${CMAKE_SYSTEM_NAME}-${CMAKE_BUILD_TYPE}-${KYDEPS}")
+
+endfunction()
