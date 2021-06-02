1f3eff0bbf6ae5f899e720f1a0e79c38b3cb0dd4
Kamen Yotov <kamen@yotov.org>
2021-06-02 09:34:24 -0400

    packages...

--- fingerprints.cmake
+++ fingerprints.cmake
@@ -1,12 +1,6 @@
 set(gflags_b97e64a9eced6112cc086269207e8165c0f695dc_sha1sum
-  610f7f98d881731df3135abb7b5370d3fbc46f6a  package/gflags_b97e64a9eced6112cc086269207e8165c0f695dc.zip
+  3f3ed8b9fc7de362e9ed96a35afd078a5f5b6ed8  package/gflags_b97e64a9eced6112cc086269207e8165c0f695dc.zip
   64-bit Windows Debug)
 set(glog_5d8872086e6acdcb08d6419d7cf8fe8e6915cc99_sha1sum
-  db5e8e9a9fe7ec4e9b66461c034b5c481d2c15f0  package/glog_5d8872086e6acdcb08d6419d7cf8fe8e6915cc99.zip
-  64-bit Windows Debug)
-set(gflags_b97e64a9eced6112cc086269207e8165c0f695dc_sha1sum
-  38e8a6237ba49986b65ed38f550b9c867e12ce6e  package/gflags_b97e64a9eced6112cc086269207e8165c0f695dc.zip
-  64-bit Windows Debug)
-set(glog_5d8872086e6acdcb08d6419d7cf8fe8e6915cc99_sha1sum
-  a55433fd4822c9d396ab1dfdb5cd660801e2e2b1  package/glog_5d8872086e6acdcb08d6419d7cf8fe8e6915cc99.zip
+  2007cab8eb18237b0fc47887b56d2bd01775bdf6  package/glog_5d8872086e6acdcb08d6419d7cf8fe8e6915cc99.zip
   64-bit Windows Debug)
