390aaa89d3b54f86962e777cf3c8388a15579fef
Kamen Yotov <kamen@yotov.org>
2021-10-12 00:12:54 -0400

    add double-conversion

---
+++ definitions/google/double-conversion.cmake
@@ -0,0 +1,6 @@
+include_guard(GLOBAL)
+
+KyDepsInstall(double-conversion
+        GIT_REPOSITORY https://github.com/google/double-conversion.git
+        GIT_REF 5fa81e88ef24e735b4283b8f7454dc59693ac1fc # v3.1.5
+)
